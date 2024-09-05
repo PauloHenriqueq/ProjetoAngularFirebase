09109c6b4a02fb74da0884865fad4aaa9db2b733
PauloHenriqueq <162391711+PauloHenriqueq@users.noreply.github.com>
2024-09-05 22:43:19 +0000

    countries

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -3,6 +3,7 @@ import { AuthenticateService } from '../services/auth.service';
 import { CrudService } from '../services/crud.service';
 import { Storage, getDownloadURL, ref, uploadBytesResumable } from '@angular/fire/storage';
 import { MessageService } from '../services/message.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -11,5 +12,34 @@ import { MessageService } from '../services/message.service';
 })
 export class HomePage {
 
-  constructor(){ }
+  paises: any;
+
+  constructor(
+    private router: Router
+  ) {
+    this.getPaisCode();
+  }
+
+  getPaisCode(){
+    fetch('https://restcountries.com/v3.1/all?fields=name,ccn3,flags')
+    .then(dados => dados.json())
+    .then(dados => {
+      console.log(dados);
+      this.paises = dados;
+    })
+    .catch(erro => {
+      console.log(erro);
+    })
+    .finally(() => {
+      console.log('processo finalizado!')
+    })
+  }
+
+  verDetalhes(ccn3: string){
+    this.router.navigate(['/ver-detalhes'], { 
+      state: { codigo: ccn3 }
+    });
+  }
+
 }
+
