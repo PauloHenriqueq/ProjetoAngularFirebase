4d9a43faa7cca94232f4d0a146608bade1c22831
PauloHenriqueq <162391711+PauloHenriqueq@users.noreply.github.com>
2024-09-26 18:38:22 +0000

    curriculo

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -10,6 +10,41 @@ import { MessageService } from '../services/message.service';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  pessoa = {
+    foto: 'https://professionalmoron.com/wp-content/uploads/2012/05/alpaca-985158_640.jpg',
+    nome: 'Barrigudinha Seleide',
+    objetivo: 'Programador HTML & CSS',
+    contato: {
+      email: 'seleide@hotmail.com',
+      telefone: '99999999',
+      github: 'gitbhub.com/seleide',
+      linkedin: 'linked.com/seleide'
+    },
+    softskills: [
+      'Comunicação',
+      'Proatividade',
+      'Trabalho em Grupo',
+      'Liderança',
+      'Resiliência',
+      'Criatividade'
+    ],
+    formacao: [
+      {
+        ano_inicio: '2023',
+        ano_termino: '2024',
+        instituicao: 'Etec Sales Gomes',
+        curso: 'Desenvolvimento de Sistemas'
+      },
+      {
+        ano_inicio: '2025',
+        ano_termino: '2028',
+        instituicao: 'Faculdade de Tecnologia de Tatuí',
+        curso: 'Análise e Desenvolvimento de Sistemas'
+      }
+    ]
 
+  }
   constructor(){ }
+
+
 }
